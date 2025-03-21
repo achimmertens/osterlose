834a703274860a213da9d701835395b430aa3123
Achim Mertens <42441871+achimmertens@users.noreply.github.com>
2025-03-21 11:22:47 +0100

    br geändert

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -45,7 +45,9 @@ const Index = () => {
               className="h-40 w-auto"
             />
           </div>
-          <h1 className="text-4xl font-bold mb-1 text-primary">Lions Club /br Bergische Löwinnen</h1>
+          <h1 className="text-4xl font-bold mb-1 text-primary">Lions Club</h1>
+          /br
+          <h1 className="text-4xl font-bold mb-1 text-primary">Bergische Löwinnen</h1>
           <h2 className="text-2xl text-primary mb-4">Osterlose</h2>
           <p className="text-lg text-muted-foreground">
             Geben Sie Ihre Losnummer ein und entdecken Sie Ihren Gewinn!
